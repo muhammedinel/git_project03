8c7eb6943d380612242bfd7bf0f83c93d545d10e
MuhammedInel <ssemeyhan@gmail.com>
2020-03-17 01:51:10 +0200

    local branch first commit

--- src/gitDay4_Package/GitDay4.java
+++ src/gitDay4_Package/GitDay4.java
@@ -3,7 +3,10 @@ package gitDay4_Package;
 public class GitDay4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+                 			
+				
+		System.out.println("Local First commit master");
+		System.out.println("Local branch first commit");
 
 	}
 
